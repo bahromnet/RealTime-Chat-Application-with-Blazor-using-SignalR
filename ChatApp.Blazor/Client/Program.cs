613b1afccfd51a36c9012f44e82639fc3693a4fe
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-07-08 05:18:24 +0300

    Add project files.

---
+++ ChatApp.Blazor/Client/Program.cs
@@ -0,0 +1,25 @@
+using ChatApp.Blazor.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace ChatApp.Blazor.Client;
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        builder.Services.AddHttpClient("ChatApp.Blazor.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+            .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+        // Supply HttpClient instances that include access tokens when making requests to the server project
+        builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("ChatApp.Blazor.ServerAPI"));
+
+        builder.Services.AddApiAuthorization();
+
+        await builder.Build().RunAsync();
+    }
+}
---
+++ ChatApp.Blazor/Server/Models/ApplicationUser.cs
@@ -0,0 +1,6 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ChatApp.Blazor.Server.Models;
+public class ApplicationUser : IdentityUser
+{
+}
