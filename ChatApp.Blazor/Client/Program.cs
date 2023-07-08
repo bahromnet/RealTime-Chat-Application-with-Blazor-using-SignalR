393ae82a7f0da448c833ce2dbac2057da1e6190f
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-07-09 00:19:25 +0300

    added some changes

--- ChatApp.Blazor/Client/Program.cs
+++ ChatApp.Blazor/Client/Program.cs
@@ -2,6 +2,8 @@ using ChatApp.Blazor.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
+using MudBlazor.Services;
 
 namespace ChatApp.Blazor.Client;
 public class Program
@@ -11,6 +13,8 @@ public class Program
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
         builder.RootComponents.Add<App>("#app");
         builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        builder.Services.AddMudServices(c => { c.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight; });
 
         builder.Services.AddHttpClient("ChatApp.Blazor.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
             .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
